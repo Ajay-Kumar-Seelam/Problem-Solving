b2848817942f367a2ba0c7557cee12af192ac428
Ajaykumarunt <ajaykumarseelam@my.unt.edu>
2023-11-13 01:27:19 -0600

    Time: 3238 ms (5.10%), Space: 19.1 MB (70.06%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        ans=""
+        ans1=[i for i in s if i not in "aeiouAEIOU"]
+        ans2=sorted([ i for i in s if i in "aeiouAEIOU"])
+
+        for i in s:
+            if i in ans1:
+                ans+=i
+
+            else:
+                ans+=ans2.pop(0)
+
+        return ans 
