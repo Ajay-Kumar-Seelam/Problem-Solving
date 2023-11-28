dd2e4ffe917f0eca196506fb29a27cbdc013aa62
Ajaykumarunt <ajaykumarseelam@my.unt.edu>
2023-11-28 15:14:27 -0600

    Time: 72 ms (15.21%), Space: 17.3 MB (6.02%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,29 +1,19 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        
-        def fun(n:int,t:int):
-            if n<1:
+        memo = [-1] * (n+1)  # Initialize memoization array
+
+        def fun(n: int):
+            if n == 1:
                 return False
-            elif n==1 and t%2==0:
-                return True
-            elif n==1 and t%2!=0:
-                return False
-            
-            for i in range(1,n):
-                
-                if i==1:
-                    return fun(n-1,t+1)
-                elif i>1 and n%i==0:
-                    fact1=i
-                    
-                    fact2=n/i
-                    if fun(n-fact1,t+1) or fun(n-fact2,t+1):
-                        return True
-                    else:
-                        return False
-        return fun(n,1)
-                    
-            
-        
-        
-        
+            if memo[n] != -1:
+                return memo[n]
+
+            for i in range(1, n):
+                if n % i == 0 and not fun(n - i):
+                    memo[n] = True
+                    return True
+
+            memo[n] = False
+            return False
+
+        return fun(n)
