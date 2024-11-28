70fe15b59648711357526e046be43fecd9f0e8bb
Ajaykumarunt <ajaykumarseelam@my.unt.edu>
2024-11-28 17:37:35 -0600

    Time: 1071 ms (82.73%), Space: 47 MB (27.15%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -4,12 +4,12 @@ class Solution:
         
         ROWS,COLS=len(grid),len(grid[0])
         
-        min_heap=[(0,0,0)]
+        q=deque([(0,0,0)]) # to store obstacles , row, column
         
         visit=set([(0,0)])
         
-        while min_heap:
-            obstacles,r,c=heapq.heappop(min_heap)
+        while q:
+            obstacles,r,c=q.popleft()
             
             if (r,c)==(ROWS-1,COLS-1):
                 return obstacles
@@ -18,8 +18,11 @@ class Solution:
             for nr,nc in nei:
                 if (nr,nc) in visit or nr<0 or nc<0 or nc==COLS or nr==ROWS:
                     continue
-                
-                heapq.heappush(min_heap,(obstacles+grid[nr][nc],nr,nc))
+                if grid[nr][nc]:
+                    q.append((obstacles+1,nr,nc))
+                else:
+                    q.appendleft((obstacles,nr,nc))
+                    
                 visit.add((nr,nc))
                 
                 
