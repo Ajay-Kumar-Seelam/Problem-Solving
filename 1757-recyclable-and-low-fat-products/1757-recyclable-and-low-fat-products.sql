534dfa67addf9a02e934b6a7283a3955d424abd2
Ajay Kumar Seelam <ajaykumarseelam@my.unt.edu>
2024-12-13 19:26:29 -0600

    Time: 646 ms (53.38%), Space: 0B (100.00%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,2 +1,5 @@
 /* Write your PL/SQL query statement below */
-select product_id from Products where low_fats='Y' and recyclable='Y';
+
+SELECT product_id
+from Products
+where low_fats='Y' and recyclable='Y';
