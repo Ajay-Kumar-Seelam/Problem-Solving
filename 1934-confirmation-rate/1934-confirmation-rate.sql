27458b88889cbbe31bed4b185fe4bf0acfdf1895
Ajay Kumar Seelam <ajaykumarseelam@my.unt.edu>
2024-12-25 07:51:04 -0600

    Time: 1055 ms (18.19%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+WITH CTE AS (
+    SELECT 
+        s.user_id, 
+        COUNT(*) AS Total, 
+        SUM(IF(c.action = 'confirmed', 1, 0)) AS ConfirmedAmount
+    FROM Signups s 
+    LEFT JOIN Confirmations c ON s.user_id = c.user_id
+    GROUP BY s.user_id
+)
+SELECT 
+    c.user_id,
+    ROUND(c.ConfirmedAmount / c.Total, 2) AS Confirmation_Rate
+FROM CTE AS c;
