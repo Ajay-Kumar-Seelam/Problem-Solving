3bb3dc56b8b1159b79437447654726fef8739605
Ajay Kumar Seelam <ajaykumarseelam@my.unt.edu>
2025-01-29 00:02:55 -0600

    Time: 39 ms (5.23%) | Memory: 45.2 MB (31.68%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,22 +1,37 @@
 class Solution {
-    int m, n, dir[] = {-1,0,1,0,-1};
+    
+    public int findMaxFish(int[][] grid) {
+        int m=grid.length,n=grid[0].length;
+        int[][] visit=new int[m][n];
+        int ans=0;
 
-    private int dfs(int[][] grid, int i, int j) {
-        if (i<0 || i>=m || j<0 || j>=n || grid[i][j]==0) return 0;
-        int sum = grid[i][j];
-        grid[i][j] = 0;
-        for (int k=0; k<4; k++)
-            sum += dfs(grid, i+dir[k], j+dir[k+1]);
+        for (int i=0;i<grid.length;i++){
+            for (int j=0;j<n;j++){
+                if (visit[i][j]==0 && grid[i][j]>0)
+               ans=Math.max(ans,dfs(grid,visit,i,j,0)) ;
+
+            }
+            System.out.println();
+        }
+        return ans;
+        }
+       
+    
+    public int dfs(int[][] grid,int[][] visit,int i,int j,int sum){
+        if(i>=grid.length || j>=grid[0].length || i<0 || j<0 || grid[i][j]==0 )
+        return 0;
+        visit[i][j]=1;
+        sum=grid[i][j];
+        if(i+1 <grid.length && visit[i+1][j]==0 && grid[i+1][j]>0) 
+        sum=sum+dfs(grid,visit,i+1,j,0);
+        if (j+1 <grid[0].length &&visit[i][j+1]==0 && grid[i][j+1]>0)
+        sum=sum+dfs(grid,visit,i,j+1,0);
+        if(i>0 && visit[i-1][j]==0 && grid[i-1][j]>0) 
+        sum=sum+dfs(grid,visit,i-1,j,0);
+        if (j>0 && visit[i][j-1]==0 && grid[i][j-1]>0)
+        sum=sum+dfs(grid,visit,i,j-1,0);
+        
         return sum;
     }
-
-    public int findMaxFish(int[][] grid) {
-        m = grid.length; n = grid[0].length;
-        int maxFish = 0;
-        for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)
-                if (grid[i][j]>0)
-                    maxFish = Math.max(maxFish, dfs(grid,i,j));
-        return maxFish;
-    }
+ 
 }
