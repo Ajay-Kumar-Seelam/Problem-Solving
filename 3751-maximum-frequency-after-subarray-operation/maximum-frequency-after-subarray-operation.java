b9d2b3ca4ddaeb977b51476e4791c47e588ad3de
Ajay Kumar Seelam <ajaykumarseelam@my.unt.edu>
2025-01-30 17:00:48 -0600

    Time: 60 ms (29.33%) | Memory: 57 MB (89.43%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -1,37 +1,35 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        Map<Integer,Integer> freqmap=new HashMap();
         
-        int maxfreq=0;
-        freqmap.put(k,0);
-        for (int i=0;i<nums.length;i++){
-            if(!freqmap.containsKey(nums[i])){
-                freqmap.put(nums[i],1);
-            }
-        else{
-            freqmap.put(nums[i],freqmap.get(nums[i])+1);
-        }
-                }
-
-        for (Integer val:freqmap.keySet()){
-            maxfreq=Math.max(maxfreq,calculate(nums,val,k));
-
-        }
-            
-        
-    return freqmap.get(k)+maxfreq;
-    }
-
-    private int calculate(int[] nums,int value,int k){
-        int maxGain=0,currentGain=0;
-
+        Map<Integer,Integer> count=new HashMap();
+        count.put(k,0);
+        int ans=0;
         for(int num:nums){
-            if(num==k) currentGain--;
-            if (num==value) currentGain++;
-            if (currentGain<0) currentGain=0;
-            maxGain=Math.max(maxGain,currentGain);
+            if(!count.containsKey(num)){
+                count.put(num,1);
+            }
+            else
+            {
+                count.put(num,count.get(num)+1);
+            }
         }
-    return maxGain;
+        for(int n:count.keySet()){
+            ans=Math.max(ans,calculate(nums,n,k));
+            //System.out.println(n+" "+k);
+        }
+        System.out.println(ans);
+    return count.get(k)+ans;
+    }
+    public int calculate(int[] nums, int value,int k){
+        int maxGain=0,curr=0;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]==k) curr=Math.max(0,curr-1);
+
+            else if(nums[i]==value) curr++;
+            maxGain=Math.max(maxGain,curr);
+        }
+        return maxGain;
 
     }
+    
 }
