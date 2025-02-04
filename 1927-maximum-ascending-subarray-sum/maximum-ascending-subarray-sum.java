aed55312017e350bd9110d08233d882dfb1488f6
Ajay Kumar Seelam <ajaykumarseelam@my.unt.edu>
2025-02-04 17:35:01 -0600

    Time: 0 ms (100.00%) | Memory: 41.3 MB (50.46%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int ans=nums[0],curr=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            
+            if(nums[i]>nums[i-1])
+            curr+=nums[i];
+
+            else{
+                curr=nums[i];
+            }
+            ans=Math.max(ans,curr);
+        }
+        
+    return ans;}
+}
